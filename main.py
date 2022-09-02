d03d0d310820cc03169f0fdc5ee5c4392dad1cbd
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 03:01:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Edit the function below
+
+# Prints personalized greetings
+def greet(name):
+  print(f"Welcome to Kibo, {name}")
+  # add two more greetings using the name parameter
+
+# The code below calls your function. Modify the line below to use your function with argument "Keno"
+greet()
+
+# Call the function two more times with two different names as arguments
