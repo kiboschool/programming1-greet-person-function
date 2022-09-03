22633fc9cd051954258a3f9887e4762678a2725c
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 23:45:35 -0600

    add solution

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# Edit the function below
-
 # Prints personalized greetings
 def greet(name):
   print(f"Welcome to Kibo, {name}")
   # add two more greetings using the name parameter
+  print(f"We're glad you're here, {name}")
+  print(f"{name}, how did you hear about us?")
 
 # The code below calls your function. Modify the line below to use your function with argument "Keno"
-greet()
+greet("Keno")
 
 # Call the function two more times with two different names as arguments
+greet("Mohammed")
+greet("Seun")
